e309c74c4b7936c999bf99eeab23e4c5057466be
kiznlh <kiznlh@gmail.com>
2024-04-13 02:23:49 +0700

    Merge branch 'main' of https://github.com/baronpm0703/TKPM-Final-Project

--- client/src/main/java/com/psvm/client/controllers/Main2.java
+++ client/src/main/java/com/psvm/client/controllers/Main2.java
@@ -1,11 +1,6 @@
 package com.psvm.client.controllers;
 
-import com.psvm.server.controllers.Main;
-
 public class Main2 {
 	public static void main(String[] args) {
-		int a = Main.test;
-		System.out.println(a);
-		int b =4324324;
 	}
 }
